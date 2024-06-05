1f80f4f572e0a477b574fa0b9e379084551e8dc9
joseguadalupe <j.060996@hotmail.com>
2024-06-05 13:15:07 -0600

    fix(RHATM0101 Autentificación): 0RHATM0101M001007 Autentificación
    se cambio el nombre de el controlador de usuarios a formato snake case

--- start/routes/login_routes.ts
+++ start/routes/login_routes.ts
@@ -3,16 +3,16 @@ import { middleware } from '../kernel.js'
 
 router
   .group(() => {
-    router.post('/', '#controllers/usercontroller.login')
-    router.post('/logout', '#controllers/usercontroller.logout').use(
+    router.post('/', '#controllers/user_controller.login')
+    router.post('/logout', '#controllers/user_controller.logout').use(
       middleware.auth({
         guards: ['api'],
       })
     )
-    router.post('/recovery', '#controllers/usercontroller.recoveryPassword')
-    router.post('/request/verify/:token', '#controllers/usercontroller.verifyRequestRecovery')
-    // router.post('/password/request', 'UserController.requestRecovery')
-    router.post('/password/reset', '#controllers/usercontroller.passwordReset')
-    // router.post('/user-auth', 'UserController.userAuth')
+    router.post('/recovery', '#controllers/user_controller.recoveryPassword')
+    router.post('/request/verify/:token', '#controllers/user_controller.verifyRequestRecovery')
+    // router.post('/password/request', 'user_controller.requestRecovery')
+    router.post('/password/reset', '#controllers/user_controller.passwordReset')
+    // router.post('/user-auth', 'user_controller.userAuth')
   })
   .prefix('/api/login')
