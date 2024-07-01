cb8f6b0a9e3ab0bd674ebffc04069a888cfe6097
Alberto Escobar <128758041+FernandoHernandezCruz@users.noreply.github.com>
2024-07-01 15:54:49 -0600

    feat(RHATM0106M003013): REST turnos

--- app/validators/shift_exception.ts
+++ app/validators/shift_exception.ts
@@ -1,7 +1,6 @@
 import Employee from '#models/employee'
 import ExceptionType from '#models/exception_type'
 import vine from '@vinejs/vine'
-import { DateTime } from 'luxon'
 
 export const createShiftExceptionValidator = vine.compile(
   vine.object({
