09c66ea3abc3dc36c727532dee99255f0f0d6182
joseguadalupesotobecerra9605 <jzoto@siler-mx.com>
2024-07-08 18:12:47 -0600

    fix(RHATM0102 Reporte de asistencia): RHATM0102M001022 Descargar Excel de asistencia por empleado
    se creo enpoint para crear el excel con la información de las asistencias por empleado y por periodo

--- app/controllers/assists_controller.ts
+++ app/controllers/assists_controller.ts
@@ -3,6 +3,10 @@ import { inject } from '@adonisjs/core'
 import { HttpContext } from '@adonisjs/core/http'
 import env from '#start/env'
 import ExcelJS from 'exceljs'
+import axios from 'axios'
+import { DateTime } from 'luxon'
+import { AssistDayInterface } from '../interfaces/assist_day_interface.js'
+import Employee from '#models/employee'
 
 export default class AssistsController {
   /**
@@ -213,45 +217,166 @@ export default class AssistsController {
       const employeeID = request.input('employeeId')
       const filterDate = request.input('date')
       const filterDateEnd = request.input('date-end')
-      let apiUrl = `${env.get('HOST')}:${env.get('PORT')}/api/v1/assists`
+      let apiUrl = `http://${env.get('HOST')}:${env.get('PORT')}/api/v1/assists`
       apiUrl = `${apiUrl}?date=${filterDate || ''}`
       apiUrl = `${apiUrl}&date-end=${filterDateEnd || ''}`
       apiUrl = `${apiUrl}&employeeId=${employeeID || ''}`
-      /*  const apiResponse = await axios.get(apiUrl)
+      const apiResponse = await axios.get(apiUrl)
       const data = apiResponse.data.data
+      //console.log(data)
+      //console.log(data)
+      const employee = await Employee.query()
+        .whereNull('employee_deleted_at')
+        .where('employee_id', employeeID)
+        .preload('position')
+        .first()
+      if (!employee) {
+        response.status(400)
+        return {
+          type: 'warning',
+          title: 'Excel assist by employee',
+          message: 'Missing data to process',
+          data: {},
+        }
+      }
+      const rows = []
       if (data) {
-        console.log(data)
-      } */
+        for await (const calendar of data.employeeCalendar) {
+          // console.log(calendar)
+          const day = this.dateDay(calendar.day)
+          const month = this.dateMonth(calendar.day)
+          const year = this.dateYear(calendar.year)
+          const calendarDay = this.calendarDay(year, month, day)
+          const weekDayName = this.weekDayName(year, month, day)
+          const firstCheck = this.chekInTime(calendar)
+          const lastCheck = this.chekOutTime(calendar)
+          const checkInTime = DateTime.fromFormat(
+            calendar.assist.dateShift.shiftTimeStart,
+            'HH:mm:ss'
+          )
+          const checkOutTime = checkInTime
+            .plus({ hours: calendar.assist.dateShift.shiftActiveHours })
+            .toFormat('ff')
+
+          rows.push({
+            name: `${employee.employeeFirstName} ${employee.employeeLastName}`,
+            position: employee.position.positionName ? employee.position.positionName : '',
+            date: calendarDay,
+            dayOfWeek: weekDayName,
+            checkInTime: checkInTime.toFormat('ff'),
+            firstCheck: firstCheck,
+            lunchTime: '',
+            returnLunchTime: '',
+            checkOutTime: checkOutTime,
+            lastCheck: lastCheck,
+            incidents: '',
+            notes: '',
+            sundayPremium: '',
+          })
+        }
+      }
       // Crear un nuevo libro de Excel
       const workbook = new ExcelJS.Workbook()
       const worksheet = workbook.addWorksheet('Datos')
 
-      // Añadir columnas
-      worksheet.columns = [
-        { header: 'Nombre', key: 'nombre', width: 30 },
-        { header: 'Cargo', key: 'cargo', width: 20 },
-        { header: 'Fecha', key: 'fecha', width: 15 },
-        { header: 'Día de Semana', key: 'dia_semana', width: 20 },
-        { header: 'Primera Checada', key: 'primera_checada', width: 20 },
-        { header: 'Última Checada', key: 'ultima_checada', width: 20 },
-        { header: 'Notas', key: 'notas', width: 30 },
-        { header: 'Prima Dominical', key: 'prima_dominical', width: 20 },
-      ]
+      // Agregar título del reporte en negritas
+      const titleRow = worksheet.addRow(['REGISTROS DE ASISTENCIA '])
+      titleRow.font = { bold: true, size: 24 }
+      titleRow.height = 20
+      titleRow.alignment = { horizontal: 'center', vertical: 'middle' }
+      worksheet.mergeCells('A1:M1')
 
-      const rows = [{}]
+      const periodRow = worksheet.addRow([`Fecha desde ${filterDate} hasta ${filterDateEnd}`])
+      periodRow.font = { size: 15 }
+      periodRow.alignment = { horizontal: 'center', vertical: 'middle' }
+      worksheet.mergeCells('A2:M2')
 
-      worksheet.addRows(rows)
+      // Añadir columnas de datos (encabezados)
+      const headerRow = worksheet.addRow([
+        'Nombre',
+        'Cargo',
+        'Fecha',
+        'Día de Semana',
+        'Hora entrada',
+        'Check Entrada',
+        'Hora salida a comer',
+        'Hora regreso de comer',
+        'Hora Salida',
+        'Check Salida',
+        'Incidencias',
+        'Notas',
+        'Prima Dominical',
+      ])
+      headerRow.font = { bold: true }
+      // Añadir filas de datos (esto es un ejemplo, puedes obtener estos datos de tu base de datos)
+      rows.forEach((rowData) => {
+        worksheet.addRow([
+          rowData.name,
+          rowData.position,
+          rowData.date,
+          rowData.dayOfWeek,
+          rowData.checkInTime,
+          rowData.firstCheck,
+          rowData.lunchTime,
+          rowData.returnLunchTime,
+          rowData.checkOutTime,
+          rowData.lastCheck,
+          rowData.incidents,
+          rowData.notes,
+          rowData.sundayPremium,
+        ])
+      })
+      const columnA = worksheet.getColumn(1)
+      columnA.width = 44
+      const columnB = worksheet.getColumn(2)
+      columnB.width = 44
+      const columnC = worksheet.getColumn(3)
+      columnC.width = 14
+      columnC.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnD = worksheet.getColumn(4)
+      columnD.width = 14
+      columnD.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnE = worksheet.getColumn(5)
+      columnE.width = 25
+      columnE.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnF = worksheet.getColumn(6)
+      columnF.width = 25
+      columnF.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnG = worksheet.getColumn(7)
+      columnG.width = 25
+      columnG.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnH = worksheet.getColumn(8)
+      columnH.width = 25
+      columnH.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnI = worksheet.getColumn(9)
+      columnI.width = 25
+      columnI.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnJ = worksheet.getColumn(10)
+      columnJ.width = 25
+      columnJ.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnK = worksheet.getColumn(11)
+      columnK.width = 30
+      columnK.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnL = worksheet.getColumn(12)
+      columnL.width = 30
+      columnL.alignment = { vertical: 'middle', horizontal: 'center' }
+      const columnM = worksheet.getColumn(13)
+      columnM.width = 30
+      columnM.alignment = { vertical: 'middle', horizontal: 'center' }
 
-      // Establecer el encabezado de respuesta para la descarga del archivo
+      // response.status(200)
+      // return true
+      // Crear un buffer del archivo Excel
+      const buffer = await workbook.xlsx.writeBuffer()
+
+      // Enviar la respuesta con el buffer del archivo Excel y la información adicional
       response.header(
         'Content-Type',
         'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
       )
       response.header('Content-Disposition', 'attachment; filename=datos.xlsx')
-
-      // Enviar el archivo Excel en la respuesta
-      await workbook.xlsx.write(response.response)
-      // return response.status(data.status).json(data)
+      response.send(buffer)
+      // return response.status(200).json(d)
     } catch (error) {
       return response.status(400).json({
         type: 'success',
@@ -260,5 +385,76 @@ export default class AssistsController {
         data: error.response || null,
       })
     }
+  }
+
+  dateYear(day: string) {
+    if (!day) {
+      return 0
+    }
+
+    const year = Number.parseInt(`${day.split('-')[0]}`)
+    return year
+  }
+
+  dateMonth(day: string) {
+    if (!day) {
+      return 0
+    }
+
+    const month = Number.parseInt(`${day.split('-')[1]}`)
+    return month
+  }
+
+  dateDay(day: string) {
+    if (!day) {
+      return 0
+    }
+
+    const dayTemp = Number.parseInt(`${day.split('-')[2]}`)
+    return dayTemp
+  }
+
+  weekDayName(dateYear: number, dateMonth: number, dateDay: number) {
+    const date = DateTime.local(dateYear, dateMonth, dateDay, 0)
+    const day = date.toFormat('cccc')
+    return day
+  }
+
+  calendarDay(dateYear: number, dateMonth: number, dateDay: number) {
+    const date = DateTime.local(dateYear, dateMonth, dateDay, 0)
+    const day = date.toFormat('DD')
+    return day
+  }
+
+  chekInTime(checkAssist: AssistDayInterface) {
+    if (!checkAssist?.assist?.checkIn?.assistPunchTimeOrigin) {
+      return ''
+    }
+
+    const time = DateTime.fromISO(checkAssist.assist.checkIn.assistPunchTimeOrigin.toString(), {
+      setZone: true,
+    })
+    const timeCST = time.setZone('UTC-5')
+    return timeCST.toFormat('ff')
+  }
+
+  chekOutTime(checkAssist: AssistDayInterface) {
+    if (!checkAssist?.assist?.checkOut?.assistPunchTimeOrigin) {
+      return ''
+    }
+
+    const now = DateTime.now().toFormat('yyyy-LL-dd')
+    const time = DateTime.fromISO(checkAssist.assist.checkOut.assistPunchTimeOrigin.toString(), {
+      setZone: true,
+    })
+    const timeDate = time.toFormat('yyyy-LL-dd')
+    const timeCST = time.setZone('UTC-5')
+
+    if (timeDate === now) {
+      checkAssist.assist.checkOutStatus = ''
+      return ''
+    }
+
+    return timeCST.toFormat('ff')
   }
 }
