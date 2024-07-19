b78fc75e50f6993471608f6ff54f9b1358963da5
joseguadalupesotobecerra9605 <jzoto@siler-mx.com>
2024-07-18 19:13:42 -0600

    fix(usabilidad usuarios): usabilidad usuarios
    se agrego el enpoint para buscar el empleado por medio del id de persona

--- app/controllers/person_controller.ts
+++ app/controllers/person_controller.ts
@@ -864,4 +864,145 @@ export default class PersonController {
       }
     }
   }
+
+  /**
+   * @swagger
+   * /api/person-get-employee/{personId}:
+   *   get:
+   *     security:
+   *       - bearerAuth: []
+   *     tags:
+   *       - Persons
+   *     summary: get employee by person id
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - in: path
+   *         name: personId
+   *         schema:
+   *           type: number
+   *         description: Person id
+   *         required: true
+   *     responses:
+   *       '200':
+   *         description: Resource processed successfully
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 type:
+   *                   type: string
+   *                   description: Type of response generated
+   *                 title:
+   *                   type: string
+   *                   description: Title of response generated
+   *                 message:
+   *                   type: string
+   *                   description: Message of response
+   *                 data:
+   *                   type: object
+   *                   description: Processed object
+   *       '404':
+   *         description: Resource not found
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 type:
+   *                   type: string
+   *                   description: Type of response generated
+   *                 title:
+   *                   type: string
+   *                   description: Title of response generated
+   *                 message:
+   *                   type: string
+   *                   description: Message of response
+   *                 data:
+   *                   type: object
+   *                   description: List of parameters set by the client
+   *       '400':
+   *         description: The parameters entered are invalid or essential data is missing to process the request
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 type:
+   *                   type: string
+   *                   description: Type of response generated
+   *                 title:
+   *                   type: string
+   *                   description: Title of response generated
+   *                 message:
+   *                   type: string
+   *                   description: Message of response
+   *                 data:
+   *                   type: object
+   *                   description: List of parameters set by the client
+   *       default:
+   *         description: Unexpected error
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 type:
+   *                   type: string
+   *                   description: Type of response generated
+   *                 title:
+   *                   type: string
+   *                   description: Title of response generated
+   *                 message:
+   *                   type: string
+   *                   description: Message of response
+   *                 data:
+   *                   type: object
+   *                   description: Error message obtained
+   *                   properties:
+   *                     error:
+   *                       type: string
+   */
+  async getEmployee({ request, response }: HttpContext) {
+    try {
+      const personId = request.param('personId')
+      if (!personId) {
+        response.status(400)
+        return {
+          type: 'warning',
+          title: 'The person Id was not found',
+          message: 'Missing data to process',
+          data: { personId },
+        }
+      }
+      const personService = new PersonService()
+      const employee = await personService.getEmployee(personId)
+      if (!employee) {
+        response.status(404)
+        return {
+          type: 'warning',
+          title: 'The employee was not found',
+          message: 'The employee was not found with the entered ID',
+          data: { personId },
+        }
+      } else {
+        response.status(200)
+        return {
+          type: 'success',
+          title: 'Persons',
+          message: 'The employee was found successfully',
+          data: { employee: employee },
+        }
+      }
+    } catch (error) {
+      response.status(500)
+      return {
+        type: 'error',
+        title: 'Server error',
+        message: 'An unexpected error has occurred on the server',
+        error: error.message,
+      }
+    }
+  }
 }
