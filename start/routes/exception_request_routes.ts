f473691d1e83806674a091aeaa7e2dc643e03e79
Alberto Escobar <128758041+FernandoHernandezCruz@users.noreply.github.com>
2024-10-30 08:31:31 -0600

    feat(SAEPB0105M001071): REST Solicitud de excepciones

--- start/routes/exception_request_routes.ts
+++ start/routes/exception_request_routes.ts
@@ -1,4 +1,5 @@
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 
 router
   .group(() => {
@@ -9,3 +10,4 @@ router
     router.get('/:id', '#controllers/exception_requests_controller.show')
   })
   .prefix('/api/exception-requests')
+  .use(middleware.auth())
