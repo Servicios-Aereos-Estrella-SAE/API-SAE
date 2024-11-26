c294554e4403bda183525978f88e4d7c813a3218
joseguadalupesotobecerra9605 <jzoto@siler-mx.com>
2024-11-26 17:59:57 -0600

    fix(SAEPB0102 Reporte de asistencia): SAEPB0102M001087 Aplicar permisos con hora
    en la tabla de tipos de excepcion se agregaron los campos de 'necesita hora de entrada' y 'necesita hora de salida' y en la tabla excepciones de turno se agrearon dos campos 'hora de entrada' y 'hora de salida'

---
+++ database/migrations/1732662862188_create_add_exception_type_need_check_out_time_to_exception_types_table.ts
@@ -0,0 +1,17 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'exception_types'
+
+  async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.tinyint('exception_type_need_check_out_time').nullable().defaultTo(0)
+    })
+  }
+
+  async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('exception_type_need_check_out_time')
+    })
+  }
+}
