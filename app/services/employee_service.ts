2e08e74dd6fc0dc62f128d3210074a029b340c52
joseguadalupesotobecerra9605 <jzoto@siler-mx.com>
2025-06-03 17:59:13 -0600

    fix(SAEPB0105 Empleados): 25SAEPB0105M006009 Empleados solo lectura
    en la consulta de empleados y descarga de excel se agrego el usuario responsable (empleado) para incuirlo en pantalla y excel

--- app/services/employee_service.ts
+++ app/services/employee_service.ts
@@ -188,8 +188,15 @@ export default class EmployeeService {
       .if(filters.userResponsibleId &&
         typeof filters.userResponsibleId && filters.userResponsibleId > 0,
         (query) => {
-          query.whereHas('userResponsibleEmployee', (userResponsibleEmployeeQuery) => {
-            userResponsibleEmployeeQuery.where('userId', filters.userResponsibleId!)
+          query.where((subQuery) => {
+            subQuery.whereHas('userResponsibleEmployee', (userResponsibleEmployeeQuery) => {
+              userResponsibleEmployeeQuery.where('userId', filters.userResponsibleId!)
+            })
+            subQuery.orWhereHas('person', (personQuery) => {
+              personQuery.whereHas('user', (userQuery) => {
+                userQuery.where('userId', filters.userResponsibleId!)
+              })
+            })
           })
         }
       )
