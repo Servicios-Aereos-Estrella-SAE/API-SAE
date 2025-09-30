167152ccce1d3cbd8e3c5eb67c0cbae58eb847b5
joseguadalupesotobecerra9605 <jzoto@siler-mx.com>
2025-09-29 19:09:59 -0600

    fix(SAEPB0102 Reporte de asistencia): 25SAEPB0102M004067 Validar modo de visualización "payrol" dinámico
    en la validacion de la tabla system setting payroll config para que el campo de dias de desfase no sea requerido

--- app/validators/system_setting_payroll_config.ts
+++ app/validators/system_setting_payroll_config.ts
@@ -6,7 +6,7 @@ export const createSystemSettingPayrollConfigValidator = vine.compile(
     systemSettingPayrollConfigFixedDay: vine.string().trim().optional(),
     systemSettingPayrollConfigFixedEveryNWeeks: vine.number().min(1).optional(),
     systemSettingPayrollConfigNumberOfDaysToBePaid: vine.number().min(1).optional(),
-    systemSettingPayrollConfigNumberOfOverdueDaysToOffset: vine.number().min(1).optional(),
+    systemSettingPayrollConfigNumberOfOverdueDaysToOffset: vine.number().optional(),
     systemSettingId: vine.number().min(1),
   })
 )
@@ -17,7 +17,7 @@ export const updateSystemSettingPayrollConfigValidator = vine.compile(
     systemSettingPayrollConfigFixedDay: vine.string().trim().optional(),
     systemSettingPayrollConfigFixedEveryNWeeks: vine.number().min(1).optional(),
     systemSettingPayrollConfigNumberOfDaysToBePaid: vine.number().min(1).optional(),
-    systemSettingPayrollConfigNumberOfOverdueDaysToOffset: vine.number().min(1).optional(),
+    systemSettingPayrollConfigNumberOfOverdueDaysToOffset: vine.number().optional(),
     systemSettingId: vine.number().min(1),
   })
 )
